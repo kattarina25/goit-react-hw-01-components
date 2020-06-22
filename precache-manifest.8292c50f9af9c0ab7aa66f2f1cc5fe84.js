b78b47f02875bb179da4e4c660e46877d86c736b
Kateryna Vodolazova <katerina.vodolazova2010@yandex.ua>
2020-06-22 23:38:47 +0300

    Updates

---
+++ precache-manifest.8292c50f9af9c0ab7aa66f2f1cc5fe84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312ef5f6a27d3f9f742bd8d958e40f4c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "9615b491983576ca98ea",
+    "url": "/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "ca3a442bffe5e106f00c",
+    "url": "/goit-react-hw-01-components/static/css/main.b6079c86.chunk.css"
+  },
+  {
+    "revision": "9615b491983576ca98ea",
+    "url": "/goit-react-hw-01-components/static/js/2.122ea750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.122ea750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3a442bffe5e106f00c",
+    "url": "/goit-react-hw-01-components/static/js/main.56ef8045.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  },
+  {
+    "revision": "005575728beaae3c0e3e6ee007f22b41",
+    "url": "/goit-react-hw-01-components/static/media/default-avatar.00557572.jpg"
+  }
+]);
